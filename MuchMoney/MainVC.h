ed7ab8f3db8a6b3b775f8f89e357f34a2da7ee0b
328714598@qq.com <apple@AppledeMBP.lan>
2016-12-17 14:58:21 +0800

    first commit

---
+++ MuchMoney/MainVC.h
@@ -0,0 +1,13 @@
+//
+//  MainVC.h
+//  MuchMoney
+//
+//  Created by Apple on 16/12/17.
+//  Copyright © 2016年 Samposn Chen. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface MainVC : BaseVC
+
+@end
